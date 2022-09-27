5ee7a4481c25fc54c742348ae8e85fa63aa30936
origin <sskkccha@gmail.com>
2022-09-27 20:02:42 +0900

    ActivityResultContracts를 활용해 Activity 이동 구현(명시적 인텐트)

---
+++ app/src/main/java/com/chahye/datatransferkt/ResultActivity.kt
@@ -0,0 +1,25 @@
+package com.chahye.datatransferkt
+
+import android.app.Activity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_result.*
+
+class ResultActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+
+        Toast.makeText(this, "message: ${intent.getStringExtra("message")}", Toast.LENGTH_SHORT).show()
+
+        button_back.setOnClickListener {
+            val intent = Intent()
+            intent.putExtra("message", "Hey, I'm ResultActivity!")
+            setResult(Activity.RESULT_OK, intent)
+            finish()
+        }
+    }
+
+}
--- app/src/main/java/com/chahye/datatransferkt/SecondFragment.kt
+++ app/src/main/java/com/chahye/datatransferkt/SecondFragment.kt
@@ -1,14 +1,30 @@
 package com.chahye.datatransferkt
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.setFragmentResultListener
 import androidx.navigation.fragment.findNavController
-import kotlinx.android.synthetic.main.fragment_first.*
+import kotlinx.android.synthetic.main.fragment_first.button
+import kotlinx.android.synthetic.main.fragment_second.*
 
 class SecondFragment : Fragment(R.layout.fragment_second) {
+
+    val getStartActivityForResult =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { activityResult ->
+            activityResult.data?.let { intent ->
+                intent.extras?.let { bundle ->
+                    Toast.makeText(
+                        requireContext(),
+                        "message: ${bundle.getString("message", "empty")}",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+        }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -21,6 +37,14 @@ class SecondFragment : Fragment(R.layout.fragment_second) {
         button.setOnClickListener {
             findNavController().navigate(R.id.action_secondFragment_to_firstFragment)
         }
+
+        button_result.setOnClickListener {
+            Intent(requireContext(), ResultActivity::class.java).apply {
+                this.putExtra("message", "Hi~ I'm SecondFragment")
+                getStartActivityForResult.launch(this)
+            }
+        }
+
     }
 
 }
