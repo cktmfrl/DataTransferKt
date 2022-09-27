f8fbb408d73851cdd6197a77a8ffc9611e8d364f
origin <sskkccha@gmail.com>
2022-09-27 17:52:48 +0900

    Initial commit

---
+++ app/src/main/java/com/chahye/datatransferkt/SecondFragment.kt
@@ -0,0 +1,19 @@
+package com.chahye.datatransferkt
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_first.*
+
+class SecondFragment : Fragment(R.layout.fragment_second) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        button.setOnClickListener {
+            findNavController().navigate(R.id.action_secondFragment_to_firstFragment)
+        }
+    }
+
+}
