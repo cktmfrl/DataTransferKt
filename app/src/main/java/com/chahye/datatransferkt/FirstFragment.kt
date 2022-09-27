cd1ada599bf1e77d46d2954e7bb9a1ee62c23ed3
origin <sskkccha@gmail.com>
2022-09-27 19:24:21 +0900

    ActivityResultContracts를 활용해 사진 가져오기 기능 구현(암시적 인텐트)

--- app/src/main/java/com/chahye/datatransferkt/FirstFragment.kt
+++ app/src/main/java/com/chahye/datatransferkt/FirstFragment.kt
@@ -2,6 +2,7 @@ package com.chahye.datatransferkt
 
 import android.os.Bundle
 import android.view.View
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.setFragmentResult
@@ -10,12 +11,21 @@ import kotlinx.android.synthetic.main.fragment_first.*
 
 class FirstFragment : Fragment(R.layout.fragment_first) {
 
+    val getContent = registerForActivityResult(ActivityResultContracts.GetContent()) {
+        imageView.setImageURI(it)
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         button.setOnClickListener {
             setFragmentResult("requestKey", bundleOf("data" to "hello"))
             findNavController().navigate(R.id.action_firstFragment_to_secondFragment)
+        }
+
+        button_photo.setOnClickListener {
+            // MIME TYPE
+            getContent.launch("image/*")
         }
 
     }
